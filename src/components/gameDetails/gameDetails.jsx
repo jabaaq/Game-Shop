30bfc2563e4ea2715908ac08247537278f50f245
Jaba <jabaqadagishvili4@gmail.com>
2023-12-11 16:46:36 +0400

    a mockup of the gameDetails component has been created

--- src/components/gameDetails/gameDetails.jsx
+++ src/components/gameDetails/gameDetails.jsx
@@ -1,8 +1,17 @@
 import "./gameDetails.css";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
 import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 import { Header } from "../header/Header";
 import { IoMdArrowRoundBack } from "react-icons/io";
-import Swiper from "swiper";
+import AccordionTable from "./accordion/accordion";
+
+import { Pagination, Navigation } from "swiper/modules";
 
 const GameDetails = () => {
   return (
@@ -15,6 +24,36 @@ const GameDetails = () => {
           </button>
           <div className="cur-game-name">
             <h1>Grand Theft Auto 5</h1>
+          </div>
+        </div>
+        <div className="details-content">
+          <div className="game-screenshots">
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={30}
+              loop={true}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Pagination, Navigation]}
+              className="mySwiper"
+            >
+              <SwiperSlide>Slide 1</SwiperSlide>
+              <SwiperSlide>Slide 2</SwiperSlide>
+              <SwiperSlide>Slide 3</SwiperSlide>
+              <SwiperSlide>Slide 4</SwiperSlide>
+              <SwiperSlide>Slide 5</SwiperSlide>
+              <SwiperSlide>Slide 6</SwiperSlide>
+              <SwiperSlide>Slide 7</SwiperSlide>
+              <SwiperSlide>Slide 8</SwiperSlide>
+              <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
+          </div>
+          <div className="game-details">
+            <div className="description-box">
+              <AccordionTable />
+            </div>
           </div>
         </div>
       </div>
