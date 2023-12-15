f93d99cc97df5f7d343495f42bd0ae629010d040
Jaba <jabaqadagishvili4@gmail.com>
2023-12-15 19:06:30 +0400

    modified cart modal

--- src/components/cartModal/cartItem/cartItem.jsx
+++ src/components/cartModal/cartItem/cartItem.jsx
@@ -1,10 +1,20 @@
 import "./cartItem.css";
+import { TfiClose } from "react-icons/tfi";
+import exampleImg from "../../../assets/img/example.jpg";
 
 const CartItem = () => {
   return (
     <div className="cart-item-container">
-      <img src="#" alt="" />
-      <div className="price"></div>
+      <div className="image-container">
+        <img src={exampleImg} className="cart-game-image" alt="Example" />
+      </div>
+      <div className="info-container">
+        <button className="exit-button">
+          <TfiClose />
+        </button>
+        <div className="game-name">Grand theft auto 5 </div>
+        <div className="added-game-price">29.99$</div>
+      </div>
     </div>
   );
 };
--- src/components/cartModal/cartModal.jsx
+++ src/components/cartModal/cartModal.jsx
@@ -5,21 +5,26 @@ const CartModal = () => {
   return (
     <div className="modal show">
       <div className="modal-box">
-        <div className="modal-content">
-          <header className="total-games">
-            <h2>3 Games</h2>
-          </header>
-          <main className="modal-games-grid">
-            <CartItem />
-            <CartItem />
-            <CartItem />
-            <CartItem />
-            <CartItem />
-            <CartItem />
-          </main>
-          <footer className="payment-information">
-            <p className="total-price">Total: $ 78.99</p>
-          </footer>
+        <div className="modal-box-inner">
+          <div className="modal-content">
+            <header className="total-games">
+              <h2>3 Games</h2>
+            </header>
+            <main className="modal-games-grid">
+              <CartItem />
+              <CartItem />
+              <CartItem />
+              <CartItem />
+              <CartItem />
+              <CartItem />
+              <CartItem />
+              <CartItem />
+              <CartItem />
+            </main>
+            <footer className="payment-information">
+              <p className="total-price">Total: $ 78.99</p>
+            </footer>
+          </div>
         </div>
       </div>
     </div>
