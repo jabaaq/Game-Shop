33ebdc20ee647f3dc2a6a05016991222ae581524
Jaba <jabaqadagishvili4@gmail.com>
2023-12-15 19:21:42 +0400

    cart modal has become responsive

--- src/components/cartModal/cartModal.jsx
+++ src/components/cartModal/cartModal.jsx
@@ -8,7 +8,7 @@ const CartModal = () => {
         <div className="modal-box-inner">
           <div className="modal-content">
             <header className="total-games">
-              <h2>3 Games</h2>
+              <h2 className="modal-game-count">3 Games</h2>
             </header>
             <main className="modal-games-grid">
               <CartItem />
