7f31aebed904d9c2081ba3202c9fc9c38413cf9e
Jaba <jabaqadagishvili4@gmail.com>
2023-12-25 17:34:48 +0400

    modified erroMessage component, added vercel configuration

--- src/components/errorMessage/errorMessage.jsx
+++ src/components/errorMessage/errorMessage.jsx
@@ -1,6 +1,7 @@
-import { RestartPageBtn } from "./restartPageBtn/restartPageBtn";
-
 import "./errorMessage.css";
+import { RestartPageBtn } from "./restartPageBtn/restartPageBtn";
+import { Link } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
 
 const ErrorMessage = () => {
   return (
@@ -8,12 +9,20 @@ const ErrorMessage = () => {
       <div className="error-modal-box">
         <h1 className="error-header-text">Oops! Something went wrong!</h1>
         <p className="error-text-paragraph">
-          You may also refresh the page or try again later
+          You may also refresh the page or try again later (Press go back)
         </p>
         <p className="error-text-paragraph">
           Brace yourself till we get the error fixed.
         </p>
-        <RestartPageBtn />
+        <div className="modal-buttons">
+          <button className="restart-button">
+            <Link to={"/"}>
+              <IoIosArrowBack />
+              <span>Go back</span>
+            </Link>
+          </button>
+          <RestartPageBtn />
+        </div>
       </div>
     </div>
   );
